14f59aa9a6fb643fed413ba3ee1718e347467dc5
ltogt <omnesiaorg@gmail.com>
2020-08-29 01:35:26 +0200

    add simple test

--- test/list_extensions_test.dart
+++ test/list_extensions_test.dart
@@ -3,11 +3,17 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:list_extensions/list_extensions.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  group("ForEachIndexed", () {
+    test('copies list with manipulated elements', () {
+      final List<String> l0 = ["a", "b", "c"];
+      final List l1 = l0.forEachIndexed((index, e) => {index: e});
+
+      expect(l1 == l0, false);
+      expect(l1, [
+        {0: "a"},
+        {1: "b"},
+        {2: "c"}
+      ]);
+    });
   });
 }
